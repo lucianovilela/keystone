a7521ecb6aeaf9f695fd345e88703e842ccfbc5b
Luciano Vilela <lucianovilela@gmail.com>
2017-07-27 16:03:01 -0300

    .

--- keystone.js
+++ keystone.js
@@ -57,7 +57,7 @@ keystone.set('nav', {
 	'galleries': 'galleries',
 	'enquiries': 'enquiries',
 	'users': 'users',
-	'agenda': ['agendas', 'organizacaoes']
+	'agenda': ['agendas', 'organizacaos']
 });
 
 // Start Keystone to connect to your database and initialise the web server
